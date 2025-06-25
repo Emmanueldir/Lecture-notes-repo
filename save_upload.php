43fb38e74357a9cec9dd2152b1fdb0becc740dcc
Emmanuel <eadiran07@gmail.com>
2025-06-25 10:15:45 +0100

    organize files by folder feature

--- save_upload.php
+++ save_upload.php
@@ -10,7 +10,16 @@ $tag = $_POST['tag'];
 
 $file = $_FILES['file'];
 $filename = basename($file['name']);
-$target = "uploads/" . $filename;
+$deptFolder = strtolower(str_replace(' ', '_', $dept));
+$semFolder = strtolower(str_replace(' ', '_', $sem));
+$uploadPath = "uploads/$deptFolder/$semFolder/";
+
+if (!file_exists($uploadPath)) {
+    mkdir($uploadPath, 0777, true);
+}
+
+$target = $uploadPath . $filename;
+#$target = "uploads/" . $filename;
 
 if (move_uploaded_file($file['tmp_name'], $target)) {
     $stmt = $conn->prepare("INSERT INTO materials (title, course_code, description, semester, department, tag, filename) VALUES (?, ?, ?, ?, ?, ?, ?)");
