aac9d594439d4844682b23cfb41f1f07c56df08e
Emmanuel <eadiran07@gmail.com>
2025-06-26 15:06:47 +0100

    added description

--- header.php
+++ header.php
@@ -2,12 +2,14 @@
 <html>
 <head>
     <title>Lecture Notes Repository</title>
-    <link rel="stylesheet" href="homestyle.css">
+    <link rel="stylesheet" href="homestyle.css?v=<?php echo time(); ?>">
 </head>
 <body>
 <div class="container">
     <h1><a href="index.php" style="text-decoration: none; color: inherit;">Lecture Notes Repository</a></h1>
     <nav style="margin-bottom: 20px;">
-        <a href="index.php">Home</a>   |
         <a href="upload.php">Upload</a>
     </nav>
+    <div class="about">
+        <p>This is simple web-based repository built with PHP and MySQL that allows users to upload, search, and download lecture notes by year, department, course code, and semester.</p>
+    
