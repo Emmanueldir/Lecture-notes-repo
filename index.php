e82f380d08841a7a35e58e0abf6f4df7864d3841
Emmanuel <eadiran07@gmail.com>
2025-06-26 15:10:08 +0100

    updated search options

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
 <form method="GET" action="search.php">
-    <input type="text" name="query" placeholder="Search by title, subject, or course code">
+    <input type="text" name="query" placeholder="Search by title, department, or course code"><br>
     <button type="submit">Search</button>
 </form>
 <?php include 'footer.php'; ?>
