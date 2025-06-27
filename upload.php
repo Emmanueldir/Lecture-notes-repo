94d89a725192ff922d079979b5fa9f4c5a818d26
Emmanuel <eadiran07@gmail.com>
2025-06-27 20:51:38 +0100

    added description to forms

--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 <?php include 'header2.php'; ?>
 <h2>Upload Lecture Material</h2>
 <form action="save_upload.php" method="POST" enctype="multipart/form-data">
-    <input type="text" name="title" placeholder="Title" required><br>
-    <input type="text" name="course_code" placeholder="Course Code" required><br>
-    <input type="text" name="department" placeholder="e.g Computer Science" required><br>
-    <input type="text" name="semester" placeholder="e.g Rain or Harmathan" required><br>
-    <input type="text" name="tag" placeholder="e.g 3rd year" required><br>
+    <label for="title">Course Title: </label><br><input type="text" name="title" placeholder="Title" required><br>
+    <label for="course_code">Course code: </label><br><input type="text" name="course_code" placeholder="Course Code" required><br>
+    <label for="department">Department: </label><br><input type="text" name="department" placeholder="e.g Computer Science" required><br>
+    <label for="semester">Semester: </label><br><input type="text" name="semester" placeholder="e.g Rain or Harmathan" required><br>
+    <label for="tag">Year: </label><br><input type="text" name="tag" placeholder="e.g 3rd year" required><br>
     <textarea name="description" placeholder="Description"></textarea><br>
     <input type="file" name="file" required><br>
     <button type="submit">Upload</button>
